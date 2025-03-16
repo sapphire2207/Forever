5b56a3c98044f7c45ddd5bf2873b655004aca869
Mysore Sridhar <118914178+sapphire2207@users.noreply.github.com>
2025-03-16 15:04:09 +0530

    Update BestSeller.jsx title

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -19,7 +19,7 @@ const BestSeller = () => {
         <div className='text-center text-3xl py-8'>
             <Title text1={'BEST'} text2={'SELLERS'} />
             <p className='w-3/4 m-auto text-sm sm:text-sm md:text-base text-gray-600'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+            Best Sellers You Can’t Resist – Grab Yours Today!
             </p>
         </div>
 
