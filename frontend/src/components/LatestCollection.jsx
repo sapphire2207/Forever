ad7fd412b0ebed7a25472a1486b936685d7ba46d
Mysore Sridhar <118914178+sapphire2207@users.noreply.github.com>
2025-03-16 15:03:05 +0530

    Update LatestCollection.jsx title

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -18,7 +18,7 @@ const LatestCollection = () => {
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTION'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+        Upgrade Your Wardrobe with Our Trendiest and Newest Collection
         </p>
       </div>
 
